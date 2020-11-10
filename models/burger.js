36c824be9d193a01466ab79322508a94906adf7f
Sean Hopkins <seahopki12@gmail.com>
2020-11-10 18:55:58 -0500

    created basic model

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,27 @@
 const orm = require("../config/orm.js");
 
-// Also inside burger.js, c
-// reate the code that will call the 
+// Also inside burger.js,
+// create the code that will call the 
 // ORM functions using burger specific 
 // input for the ORM.
+
+const burger = {
+    selectAll: function(cb) {
+      orm.selectAll(function(res) {
+        console.log(res);
+      });
+    },
+    // The variables cols and vals are arrays.
+    create: function(col, val, cb) {
+      orm.create(col, val, function(res) {
+        console.log(res);
+      });
+    },
+    update: function(val, condition, cb) {
+      orm.update(val, condition, function(res) {
+        console.log(res);
+      });
+    }
+  };
 
 module.exports = burger;
